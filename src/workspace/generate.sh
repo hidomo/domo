6343624376a0aac573187550021e6dbc20376ad6
fancl20 <fancl20@gmail.com>
2019-04-18 14:52:29 +0800

    update workspace

--- src/workspace/generate.sh
+++ src/workspace/generate.sh
@@ -4,11 +4,12 @@ workspaces=$(ls -d /Users/fancl20/Workspace/*/|xargs|tr ' ' ',')
 tmpfss=$(eval "echo '--tmpfs '{${workspaces}}build:exec")
 
 for cmd in ${cmds}; do
-    echo '#!/bin/bash'$'\n' \
-         "export DOMO_RUN_ARGS=\"--privileged --net=host ${tmpfss}\""$'\n' \
+    echo '#!/bin/bash' > ./bin/workspace/$cmd
+    if [[ ${cmd} == "make" ]]; then
+        cat ./rsync.sh >> ./bin/workspace/make
+    fi
+    echo "export DOMO_RUN_ARGS=\"--privileged --net=host ${tmpfss}\""$'\n' \
          'export DOMO_CMD=workspace'$'\n' \
-         'domoExec '$cmd' "$@"' > ./bin/workspace/$cmd
+         'domoExec '$cmd' "$@"' >> ./bin/workspace/$cmd
     chmod +x ./bin/workspace/$cmd
 done
-
-cat ./rsync.sh >> ./bin/workspace/make
